f64dfa5af00206ffa794673e0e86aa8adcff95fa
Derious V <rosselijahvaughn@gmail.com>
2026-01-08 11:45:37 -0800

    chore: add events-scoped notion wrapper scaffold

---
+++ src/apps/events/notion.ts
@@ -0,0 +1,30 @@
+import { loadConfig } from "../../lib/config";
+import { createNotionClientPool } from "../../lib/notion/clientPool";
+import { createNotionApi } from "../../lib/notion/api";
+
+let api: ReturnType<typeof createNotionApi> | null = null;
+
+function getApi(): ReturnType<typeof createNotionApi> {
+  if (api) return api;
+
+  const config = loadConfig();
+  const pool = createNotionClientPool({
+    name: "events",
+    tokens: config.events.notionTokens,
+    notionVersion: config.notionVersion,
+  });
+
+  api = createNotionApi((options) => pool.request(options));
+  return api;
+}
+
+export const queryDatabase = (...args: Parameters<ReturnType<typeof createNotionApi>["queryDatabase"]>) =>
+  getApi().queryDatabase(...args);
+
+export const createPage = (...args: Parameters<ReturnType<typeof createNotionApi>["createPage"]>) =>
+  getApi().createPage(...args);
+
+export const getPage = (...args: Parameters<ReturnType<typeof createNotionApi>["getPage"]>) =>
+  getApi().getPage(...args);
+
+
