6648f102be881a104435ff37e5ac5e87b3cd46f2
Derious V <rosselijahvaughn@gmail.com>
2026-01-22 15:06:37 -0800

    Refactor HTTP wiring into reusable router

---
+++ src/exports.ts
@@ -0,0 +1,3 @@
+export { createDispatchEventsRouter } from "./http/createDispatchEventsRouter";
+export { handleDispatchWebhook } from "./apps/dispatch/handler";
+export { handleEventsWebhook } from "./apps/events/handler";
